d9907bcf6826cfc9dd763831cd2f7851096a4e58
jwcomplete <68453307+jwcomplete@users.noreply.github.com>
2026-01-19 09:23:28 -0500

    Fix loan_limits dictionary syntax error

--- home_affordability_calculator (7).py
+++ home_affordability_calculator (7).py
@@ -29,7 +29,7 @@ loan_formulas = {
 }
 
 # **Loan Limits for Conforming & High-Balance Loans**
-loan_limits = {"conforming": 832750, "high_balance": 1249129},
+loan_limits = 1: {"conforming": 832750, "high_balance": 1249129},
     2: {"conforming": 1066250, "high_balance": 1599375},
     3: {"conforming": 1288800, "high_balance": 1933200},
     4: {"conforming": 1601750, "high_balance": 2402625}
